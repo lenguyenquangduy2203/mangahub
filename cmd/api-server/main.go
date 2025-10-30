5144f98ceb077a2e4e3eaccb2c0fd7e120c8df5b
huyshin86 <huy.nguyen04@outlook.com>
2025-10-31 00:23:55 +0700

    Add API doc, loadEnv.go, go.mod and go.work for root project. Initiate "gin" server. Modify every go.mod in cmd/ to use go1.24.9 for development.

--- cmd/api-server/main.go
+++ cmd/api-server/main.go
@@ -1,5 +1,35 @@
 package main
 
+import (
+	"net/http"
+	"os"
+
+	"mangahub/pkg/utils/initializers"
+
+	"github.com/gin-gonic/gin"
+)
+
+func init() {
+	initializers.LoadEnv()
+}
+
 func main() {
-	println("Running API Server")
+	router := gin.Default()
+
+	// version 1
+	apiV1 := router.Group("/v1")
+
+	apiV1.GET("/health", func(ctx *gin.Context) {
+		ctx.JSON(http.StatusOK, gin.H{
+			"message": "good",
+		})
+	})
+
+	// Read port from env
+	port := os.Getenv("API_PORT")
+	if port == "" {
+		port = "3000" // fallback default
+	}
+
+	router.Run(":" + port)
 }
--- cmd/udp-server/go.mod
+++ cmd/udp-server/go.mod
@@ -1,3 +1,3 @@
 module udp-server
 
-go 1.25.1
+go 1.24.9
