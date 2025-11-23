d1faa2963c485f2cb9ed654905def09f0403e4af
heineken <lenguyenquanduy@gmail.com>
2025-11-23 15:49:59 +0700

    Implement udp server startup and manage clients feature

--- internal/udp/server/server.go
+++ internal/udp/server/server.go
@@ -1,12 +1,8 @@
 package server
 
 import (
-	"encoding/json"
-	"fmt"
 	"log"
 	"net"
-
-	"mangahub/internal/udp/models"
 )
 
 type NotificationServer struct {
@@ -25,8 +21,6 @@ func (s *NotificationServer) Listen() error {
 		return err
 	}
 	defer conn.Close()
-
-	fmt.Printf("UDP Notification Broadcast Server started on port %s\n", s.Port)
 
 	buf := make([]byte, 1024)
 	for {
@@ -51,25 +45,4 @@ func (s *NotificationServer) addClient(client net.UDPAddr) {
 		}
 	}
 	s.Clients = append(s.Clients, client)
-}
-
-func (s *NotificationServer) Broadcast(notification models.Notification) {
-	data, err := json.Marshal(notification)
-	if err != nil {
-		log.Println("JSON marshal error:", err)
-		return
-	}
-
-	for _, client := range s.Clients {
-		conn, err := net.DialUDP("udp", nil, &client)
-		if err != nil {
-			log.Println("Dial error:", err)
-			continue
-		}
-		_, err = conn.Write(data)
-		if err != nil {
-			log.Println("Write error:", err)
-		}
-		conn.Close()
-	}
 }
