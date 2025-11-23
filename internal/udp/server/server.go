3796eae03f6af3a5bcf0cad344c6abff5ca1f363
heineken <lenguyenquanduy@gmail.com>
2025-11-23 15:49:59 +0700

    Implement udp server startup and manage clients feature

--- internal/udp/server/server.go
+++ internal/udp/server/server.go
@@ -1,8 +1,48 @@
 package server
 
-import "net"
+import (
+	"log"
+	"net"
+)
 
 type NotificationServer struct {
 	Port    string
 	Clients []net.UDPAddr
+}
+
+func (s *NotificationServer) Listen() error {
+	addr, err := net.ResolveUDPAddr("udp", ":"+s.Port)
+	if err != nil {
+		return err
+	}
+
+	conn, err := net.ListenUDP("udp", addr)
+	if err != nil {
+		return err
+	}
+	defer conn.Close()
+
+	buf := make([]byte, 1024)
+	for {
+		n, clientAddr, err := conn.ReadFromUDP(buf)
+		if err != nil {
+			log.Println("Read error:", err)
+			continue
+		}
+
+		msg := string(buf[:n])
+		if msg == "register" {
+			s.addClient(*clientAddr)
+			log.Println("Registered client:", clientAddr)
+		}
+	}
+}
+
+func (s *NotificationServer) addClient(client net.UDPAddr) {
+	for _, c := range s.Clients {
+		if c.IP.Equal(client.IP) && c.Port == client.Port {
+			return // already registered
+		}
+	}
+	s.Clients = append(s.Clients, client)
 }
