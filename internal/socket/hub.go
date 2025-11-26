c2377ed6fa7f6cec38cadb77bedd30c735596e62
huyshin86 <huy.nguyen04@outlook.com>
2025-11-26 20:32:34 +0700

    Add base codes for socket service.

---
+++ internal/socket/hub.go
@@ -0,0 +1,44 @@
+package socket
+
+type Hub struct {
+	// Register clients
+	clients map[*Client]bool
+	// Comming message from clients
+	broadcast chan []byte
+	// Register rq from clients
+	register chan *Client
+	// Unregister rq from clients
+	unregister chan *Client
+}
+
+func newHub() *Hub {
+	return &Hub{
+		clients:    make(map[*Client]bool),
+		broadcast:  make(chan []byte),
+		register:   make(chan *Client),
+		unregister: make(chan *Client),
+	}
+}
+
+func (h *Hub) run() {
+	for {
+		select {
+		case client := <-h.register:
+			h.clients[client] = true
+		case client := <-h.unregister:
+			if _, ok := h.clients[client]; ok {
+				delete(h.clients, client)
+				close(client.send)
+			}
+		case message := <-h.broadcast:
+			for client := range h.clients {
+				select {
+				case client.send <- message:
+				default:
+					close(client.send)
+					delete(h.clients, client)
+				}
+			}
+		}
+	}
+}
