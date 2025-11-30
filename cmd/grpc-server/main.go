f5de8a65ff98d39636dda28f1cf22b8078806bb5
heineken <lenguyenquanduy@gmail.com>
2025-11-30 16:06:26 +0700

    Refactor gRPC server Dockerfile and main.go; add MangaServiceServer implementation

--- cmd/grpc-server/main.go
+++ cmd/grpc-server/main.go
@@ -1,5 +1,56 @@
 package main
 
+import (
+	"log"
+	grpcsrv "mangahub/internal/grpc"
+	"mangahub/internal/mangas"
+	"mangahub/internal/users"
+	"mangahub/pkg/utils/config"
+	"mangahub/pkg/utils/database"
+	mangapb "mangahub/proto/manga"
+	"net"
+
+	"google.golang.org/grpc"
+)
+
 func main() {
-	println("Running gRPC Server")
+	// Load Configuration
+	cfg, err := config.LoadConfig()
+	if err != nil {
+		log.Fatalf("Failed to load configuration: %v", err)
+	}
+
+	// Read port from env
+	// port := cfg.PORT
+
+	lis, err := net.Listen("tcp", ":50051")
+	if err != nil {
+		log.Fatalf("Failed to listen: %v", err)
+	}
+
+	s := grpc.NewServer()
+
+	// Initialize Database Connection
+	dbConnector, err := database.NewDatabaseConnection(cfg.DB_PATH)
+	if err != nil {
+		log.Fatalf("Failed to connect to database: %v", err)
+	}
+	// Defer the close on the underlying SQL connection
+	sqlConnection := dbConnector.(*database.Database).SQLDB()
+	defer sqlConnection.Close()
+
+	// Dependency Injection and Wiring
+	// Repos
+	userRepo := users.NewRepository(dbConnector)
+	mangaRepo := mangas.NewRepository(dbConnector)
+
+	// Service
+	mangaService := grpcsrv.NewMangaServiceGrpcServer(*userRepo, *mangaRepo)
+
+	mangapb.RegisterMangaServiceServer(s, mangaService)
+
+	log.Println("gRPC server running at :50051")
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("Failed to serve: %v", err)
+	}
 }
